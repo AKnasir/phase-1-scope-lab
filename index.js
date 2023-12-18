22bd3e20df6b52fc5196e88507a836741f1cb9a3
AKnasir <152478375+AKnasir@users.noreply.github.com>
2023-12-18 09:57:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'John';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    setBestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer() {
+    setBestCustomer = 'still not bob';
+  }
+  const leastFavoriteCustomer = 'Alice';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob';
+}   
